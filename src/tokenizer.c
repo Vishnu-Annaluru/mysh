5c1f45776e64d1ebaf13e7c27b5c2e82f707c37e
Vishnu <vishnu.annaluru@gmail.com>
2025-04-15 19:40:23 -0400

    Final version tokenizer.c

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -3,23 +3,25 @@
 #include <string.h>
 #include <stdio.h>
 
-arraylist_t getCommandList(char *str) {
+arraylist_t getCommandList(char *str){
     arraylist_t list;
     al_init(&list, 10);
     
-    // Remove comments: ignore everything after '#'
+    // removing comments, ignoring everything after #
     char *comment = strchr(str, '#');
-    if (comment != NULL) {
+    if(comment != NULL){
         *comment = '\0';
     }
     
     char *token = strtok(str, " \t\n");
-    while (token != NULL) {
+    while(token != NULL){
+        
         char *tokenCopy = malloc(strlen(token) + 1);
-        if (tokenCopy == NULL) {
+        if(tokenCopy == NULL){
             perror("malloc failed");
             return list;
         }
+        
         strcpy(tokenCopy, token);
         al_append(&list, tokenCopy);
         token = strtok(NULL, " \t\n");
