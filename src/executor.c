d72008477787fd1bea673df6fbde2ef1d423556a
Vishnu <vishnu.annaluru@gmail.com>
2025-04-15 19:30:29 -0400

    Final version executor.c

--- src/executor.c
+++ src/executor.c
@@ -12,172 +12,197 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-void executeCommand(arraylist_t *list, int *index, int end_idx) {
-    // If no end_idx provided (default value -1), use list length
-    if (end_idx == -1) {
-        end_idx = list->length;
+// Function to execute a command, start and end are the index of the command in the list
+void executeCommand(arraylist_t *list, int *start, int end){
+    
+    // If no end provided (default value -1), use list length
+    if(end == -1){
+        end = list->length;
     }
     
-    if (*index >= end_idx) {
+    if(*start >= end){
         return;
     }
     
-    char *command = list->data[*index];
-    (*index)++;
+    char *command = list->data[*start];
+    (*start)++;
     
-    // Handle built-in commands (cd, which, pwd)
-    if (strcmp(command, "cd") == 0) {
-        if (*index < end_idx) {
-            char *path = list->data[*index];
-            (*index)++;
+    // Built-in commands
+    if(strcmp(command, "cd") == 0){
+        if(*start < end){
+            char *path = list->data[*start];
+            (*start)++;
             cd(path);
-        } else {
-            fprintf(stderr, "cd: missing argument\n");
         }
-    } else if (strcmp(command, "which") == 0) {
-        if (*index < end_idx) {
-            char *cmd = list->data[*index];
+        else{
+            fprintf(stderr, "cd: wrong number of arguments\n");
+        }
+    }
+    else if(strcmp(command, "which") == 0){
+        
+        if(*start < end){
+            
+            char *cmd = list->data[*start];
             char *path = which(cmd);
-            if (path != NULL) {
+            if(path != NULL){
                 printf("%s\n", path);
                 fflush(stdout);
                 free(path);
             }
-            (*index)++;
+            (*start)++;
         }
-    } else if (strcmp(command, "pwd") == 0) {
+    }
+    else if(strcmp(command, "pwd") == 0){
+        
         char *directory = pwd();
-        if (directory != NULL) {
+        if(directory != NULL){
             printf("%s\n", directory);
             fflush(stdout);
             free(directory);
-        } else {
+        }
+        else{
             perror("pwd failed");
         }
-    } else if (strcmp(command, "die") == 0) {
-        // Extract arguments for die
-        int arg_count = end_idx - *index;
-        char **args = malloc((arg_count + 1) * sizeof(char *));
-        if (!args) {
-            perror("Memory allocation failed");
+    }
+    else if(strcmp(command, "die") == 0){
+
+        int argc = end - *start;
+
+        char **args = malloc((argc + 1) * sizeof(char *));
+        if(!args){
+            perror("malloc failed");
             return;
         }
         
-        // Copy arguments
-        for (int i = 0; i < arg_count; i++) {
-            args[i] = list->data[*index + i];
+        for(int i = 0; i < argc; i++){
+            args[i] = list->data[*start + i];
         }
-        args[arg_count] = NULL;
+        args[argc] = NULL;
         
-        // Call die with arguments
-        die(args, arg_count);
+        die(args, argc); // this will exit
         
-        // Note: die() will exit, so this code won't be reached
         free(args);
-    } else {
-        // External command execution
+    }
+    else{ // Everything else, organize later
+        
         char *path;
-        if (strchr(command, '/') != NULL) {
+        if(strchr(command, '/') != NULL){
+            
             path = malloc(strlen(command) + 1);
-            if (path == NULL) {
-                perror("Memory allocation failed");
+            if(path == NULL){
+                perror("malloc failed");
                 return;
             }
+            
             strcpy(path, command);
-        } else {
+        }
+        else{
             path = which(command);
-            if (path == NULL)
+            if(path == NULL)
                 return;
         }
         
-        // Parse arguments and handle redirection
+        // Redirection
+        
         char *infile = NULL;
         char *outfile = NULL;
-        int arg_count = 0;
-        int i = *index;
+        int argc = 0;
+        int i = *start;
         
-        // First pass: count actual arguments and find redirection
-        while (i < end_idx) {
-            if (strcmp(list->data[i], "<") == 0 && i + 1 < end_idx) {
+        while(i < end){
+
+            if(strcmp(list->data[i], "<") == 0 && i + 1 < end){
+                
                 infile = list->data[i + 1];
-                i += 2; // Skip redirection token and filename
-            } else if (strcmp(list->data[i], ">") == 0 && i + 1 < end_idx) {
+                i += 2;
+            }
+            else if(strcmp(list->data[i], ">") == 0 && i + 1 < end){
                 outfile = list->data[i + 1];
-                i += 2; // Skip redirection token and filename
-            } else {
-                arg_count++;
+                i += 2;
+            }
+            else{
+                argc++;
                 i++;
             }
         }
         
-        // Allocate argument array
-        char **args = malloc((arg_count + 2) * sizeof(char *));
-        if (args == NULL) {
+        
+        char **args = malloc((argc + 2) * sizeof(char *));
+        if(args == NULL){
             free(path);
-            perror("Memory allocation failed");
+            perror("malloc failed");
             return;
         }
         
-        // Second pass: fill argument array (skipping redirection)
+
+
+
         args[0] = command;
-        int arg_idx = 1;
-        i = *index;
+        int index = 1;
+        i = *start;
         
-        while (i < end_idx && arg_idx <= arg_count) {
-            if ((strcmp(list->data[i], "<") == 0 || 
-                 strcmp(list->data[i], ">") == 0) && i + 1 < end_idx) {
-                i += 2; // Skip redirection token and filename
-            } else {
-                args[arg_idx++] = list->data[i++];
+        while(i < end && index <= argc){
+            if(((strcmp(list->data[i], "<") == 0) || (strcmp(list->data[i], ">") == 0)) && (i + 1 < end)){
+                i += 2;
+            }
+            else{
+                args[index++] = list->data[i++];
             }
         }
-        args[arg_idx] = NULL;
+
+        args[index] = NULL;
         
-        // Fork and execute
+        // Fork proceses and execute them
+
         pid_t pid = fork();
-        if (pid < 0) {
-            perror("Fork failed");
+        if(pid < 0){
+            perror("fork failed");
             free(path);
             free(args);
             return;
-        } else if (pid == 0) {
+        }
+        else if(pid == 0){
+            
             // Child process
             
-            // Set up input redirection
-            if (infile != NULL) {
+            // input
+            if(infile != NULL){
+                
                 int fd = open(infile, O_RDONLY);
-                if (fd == -1) {
-                    perror("Failed to open input file");
+                if(fd == -1){
+                    perror("failed to open input file");
                     exit(EXIT_FAILURE);
                 }
+                
                 dup2(fd, STDIN_FILENO);
                 close(fd);
             }
             
-            // Set up output redirection
-            if (outfile != NULL) {
+            // output
+            if(outfile != NULL){
+                
                 int fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0640);
-                if (fd == -1) {
+                if(fd == -1){
                     perror("Failed to open output file");
                     exit(EXIT_FAILURE);
                 }
+                
                 dup2(fd, STDOUT_FILENO);
                 close(fd);
             }
             
             execv(path, args);
             
-            // If execv returns, there was an error
             perror("execv failed");
             free(path);
             free(args);
             exit(EXIT_FAILURE);
-        } else {
+
+        }
+        else{ // Parent process needs to wait 
             int status;
             waitpid(pid, &status, 0);
-            
-            // Update index to skip all processed arguments
-            *index = end_idx; // All arguments have been processed
+            *start = end;
         }
         
         free(path);
@@ -185,45 +210,50 @@ void executeCommand(arraylist_t *list, int *index, int end_idx) {
     }
 }
 
-void executePipeline(arraylist_t *list, int start_idx, int pipe_idx, int end_idx) {
+void executePipeline(arraylist_t *list, int start_index, int pipe_idx, int end){
+    
     int pipefd[2];
-    if (pipe(pipefd) == -1) {
+    if(pipe(pipefd) == -1){
         perror("pipe failed");
         return;
     }
 
+    // First
     pid_t pid1 = fork();
-    if (pid1 < 0) {
+    if(pid1 < 0){
         perror("fork failed");
         close(pipefd[0]); close(pipefd[1]);
         return;
-    } else if (pid1 == 0) {
-        // First command: stdout -> pipe
+    }
+    else if(pid1 == 0){
+        
         close(pipefd[0]);
         dup2(pipefd[1], STDOUT_FILENO);
         close(pipefd[1]);
-        int idx = start_idx;
-        executeCommand(list, &idx, pipe_idx);
+
+        int index = start_index;
+        executeCommand(list, &index, pipe_idx);
         
-        // Important: Child process must exit after command execution
-        // This prevents memory leaks by ensuring we don't return to the main loop
+        // have to exit
         exit(EXIT_SUCCESS);
     }
 
+    // Second
     pid_t pid2 = fork();
-    if (pid2 < 0) {
+    if(pid2 < 0){
         perror("fork failed");
         close(pipefd[0]); close(pipefd[1]);
         return;
-    } else if (pid2 == 0) {
-        // Second command: stdin <- pipe
+    }
+    else if(pid2 == 0){
+        
         close(pipefd[1]);
         dup2(pipefd[0], STDIN_FILENO);
         close(pipefd[0]);
-        int idx = pipe_idx + 1;
-        executeCommand(list, &idx, end_idx);
         
-        // Important: Child process must exit after command execution
+        int commandIndex = pipe_idx + 1;
+        executeCommand(list, &commandIndex, end);
+        
         exit(EXIT_SUCCESS);
     }
 
@@ -231,92 +261,108 @@ void executePipeline(arraylist_t *list, int start_idx, int pipe_idx, int end_idx
     close(pipefd[1]);
     waitpid(pid1, NULL, 0);
     waitpid(pid2, NULL, 0);
+
 }
 
-// Helper function to duplicate a string without using strdup
-char* duplicate_string(const char* str) {
-    size_t len = strlen(str) + 1; // +1 for null terminator
-    char* new_str = malloc(len);
-    if (new_str == NULL) return NULL;
-    strcpy(new_str, str);
-    return new_str;
+
+static char* duplicateString(char* str){
+    size_t len = strlen(str) + 1;
+    
+    char* temp = malloc(len);
+    if(temp == NULL){
+        perror("malloc failed");
+        return NULL;
+    }
+    
+    strcpy(temp, str);
+    return temp;
 }
 
-// Revised function to process wildcards in token list
-void processWildcards(arraylist_t *list) {
-    arraylist_t new_list;
-    al_init(&new_list, list->length);
+static void executeWildcard(arraylist_t *list){
 
-    for (int i = 0; i < list->length; i++) {
+    arraylist_t newList;
+    al_init(&newList, list->length);
+
+    int i;
+    for(i = 0; i < list->length; i++){
         char *token = list->data[i];
 
-        // Don't expand wildcards in special tokens like |, <, >
-        if (strcmp(token, "|") == 0 ||
-            strcmp(token, "<") == 0 ||
-            strcmp(token, ">") == 0) {
-            // Always duplicate, then free the original
-            char *new_token = duplicate_string(token);
-            if (new_token) {
-                al_append(&new_list, new_token);
+        if(strcmp(token, "|") == 0 || strcmp(token, "<") == 0 || strcmp(token, ">") == 0){
+            
+            // was getting memory leaks
+            char *temp = duplicateString(token);
+            if(temp){
+                al_append(&newList, temp);
             }
+
             free(token);
             continue;
+
         }
 
-        // If token contains a wildcard, expand it
-        if (strchr(token, '*') != NULL) {
+        // 'expanding' wildcard glob guy
+        if(strchr(token, '*') != NULL){
+            
             arraylist_t expanded;
             al_init(&expanded, 10);
 
             expandWildcard(token, &expanded);
+            
 
-            if (expanded.length > 0) {
-                for (int j = 0; j < expanded.length; j++) {
-                    al_append(&new_list, expanded.data[j]);
-                }
-            } else {
-                // No matches, duplicate the original token
-                char *new_token = duplicate_string(token);
-                if (new_token) {
-                    al_append(&new_list, new_token);
+            if(expanded.length > 0){
+                int j;
+                for(j = 0; j < expanded.length; j++){
+                    al_append(&newList, expanded.data[j]);
                 }
             }
+            else{ // Add original if there are no matches to the arugment list
+                char *temp = duplicateString(token);
+                if(temp){
+                    al_append(&newList, temp);
+                }
+            }
+            //al_destroy(&expanded);
             free(expanded.data);
-            free(token); // Always free the original after expansion
-        } else {
-            // No wildcard, duplicate and free the original
-            char *new_token = duplicate_string(token);
-            if (new_token) {
-                al_append(&new_list, new_token);
+            free(token);
+        }
+        else{
+
+            char *temp = duplicateString(token);
+            if(temp){
+                al_append(&newList, temp);
             }
             free(token);
         }
     }
 
     free(list->data);
-    list->data = new_list.data;
-    list->length = new_list.length;
-    list->capacity = new_list.capacity;
+    list->data = newList.data;
+    list->length = newList.length;
+    list->capacity = newList.capacity;
+
 }
 
-// Modify executeCommands to process wildcards first
-void executeCommands(arraylist_t *list) {
-    // Process wildcards before executing commands
-    processWildcards(list);
+void executeCommands(arraylist_t *list){
     
-    // Rest of the function remains the same
-    int pipe_idx = -1;
-    for (int i = 0; i < list->length; i++) {
-        if (strcmp(list->data[i], "|") == 0) {
-            pipe_idx = i;
+    // Wildcard first
+    executeWildcard(list);
+    int i;
+
+    
+    int index = -1;
+
+    for(i = 0; i < list->length; i++){
+        if(strcmp(list->data[i], "|") == 0){
+            index = i;
             break;
         }
     }
     
-    if (pipe_idx >= 0)
-        executePipeline(list, 0, pipe_idx, list->length);
-    else {
-        int idx = 0;
-        executeCommand(list, &idx, -1);  // -1 means use list->length
+    if(index >= 0){
+        executePipeline(list, 0, index, list->length);
+    }
+    else{
+        int index = 0;
+        executeCommand(list, &index, -1);
     }
 }
