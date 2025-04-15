63246b6b27372c877fe91b67b79d50307b411c44
Vishnu <vishnu.annaluru@gmail.com>
2025-04-15 19:40:09 -0400

    Final version cd.c

--- src/cd.c
+++ src/cd.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void cd(char* path) {
+// Change working directory using chdir()
+void cd(char* path){
     int c = chdir(path);
     if(c != 0){
         perror("cd");
